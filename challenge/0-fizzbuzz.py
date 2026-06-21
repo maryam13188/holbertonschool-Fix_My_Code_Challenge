3eabf24fe7123b01639e79666efc89d8e6c59f5c
maryam13188 <roro13188@gmail.com>
2026-06-21 19:27:06 +0300

    Create 0-fizzbuzz.py

---
+++ challenge/0-fizzbuzz.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+
+
+def fizzbuzz(n):
+    result = []
+
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(str(i))
+
+    print(" ".join(result))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        sys.exit(1)
+
+    fizzbuzz(int(sys.argv[1]))
